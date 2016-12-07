cf3bafffbe0dcdbc721f96a784171a19211c87e3
Xiaowen Fang <164426683@qq.com>
2016-12-06 21:48:21 -0500

    Update DatabaseTest.java

--- app/src/androidTest/java/com/cs321/todolist/DatabaseTest.java
+++ app/src/androidTest/java/com/cs321/todolist/DatabaseTest.java
@@ -15,9 +15,8 @@ import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.util.Log;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.*;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
@@ -51,18 +50,18 @@ public class DatabaseTest {
     public void cleanDatabse(){
         myDatabase.delete(TaskContract.TaskEntry.TABLE, null, null);
     }
-    
+
     @After
     public void finish() {
         myDatabase.close();
     }
 
-    @Test  //can we create new database?
+    @Test   //can we create a new databse?
     public void testPreConditions() {
         assertNotNull(myDatabase);
     }
 
-    @Test   //can we add new task?
+    @Test   //can we add tasks?
     public void addTasks(){
         String s = "Landry";
         int priority = 1;
@@ -83,7 +82,7 @@ public class DatabaseTest {
 
         Cursor cursor = myDatabase.query(TaskContract.TaskEntry.TABLE,
                 new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE, TaskContract.TaskEntry.COL_TASK_PRIORITY},
-                null, null, null, null, TaskContract.TaskEntry.COL_TASK_PRIORITY + " DESC");
+                null, null, null, null, TaskContract.TaskEntry.COL_TASK_PRIORITY );
         //get the task title and task priority and store them in taskList and priorityList
         while(cursor.moveToNext()) {
             int idx1 = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
@@ -93,14 +92,107 @@ public class DatabaseTest {
         }
 
         String s2 = taskList.get(0);
-        Log.i("S2 isiiiiiiiiiiiiii ", s2);
         assertEquals(s2, s);
         int priority2 = priorityList.get(0);
-        assertTrue(priority2 == 1);
+        assertTrue(priority2 == priority);
 
-        myDatabase.delete(TaskContract.TaskEntry.TABLE,
-                TaskContract.TaskEntry.COL_TASK_TITLE + " = ?",
-                new String[]{s});
+        myDatabase.delete(TaskContract.TaskEntry.TABLE, null, null);
+    }
+
+    @Test   //Ordered by priority??
+    public void priorityOrder(){
+        String tasks[] = {"Landry", "Final Report", "Pay my bill", "CS321 Test", "Gym", "Sleep"};
+        int priorities[] = {0, 3, 1, 2, 3, 1};
+        ContentValues values = new ContentValues();
+
+        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, "Landry");
+        values.put(TaskContract.TaskEntry.COL_TASK_PRIORITY, 0);
+        myDatabase.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                null,
+                values,
+                SQLiteDatabase.CONFLICT_REPLACE);
+
+        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, "Final Report");
+        values.put(TaskContract.TaskEntry.COL_TASK_PRIORITY, 3);
+        myDatabase.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                null,
+                values,
+                SQLiteDatabase.CONFLICT_REPLACE);
+
+        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, "Pay my bill");
+        values.put(TaskContract.TaskEntry.COL_TASK_PRIORITY, 1);
+        myDatabase.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                null,
+                values,
+                SQLiteDatabase.CONFLICT_REPLACE);
+
+        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, "Sleep");
+        values.put(TaskContract.TaskEntry.COL_TASK_PRIORITY, 1);
+        myDatabase.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                null,
+                values,
+                SQLiteDatabase.CONFLICT_REPLACE);
+
+        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, "CS321 Test");
+        values.put(TaskContract.TaskEntry.COL_TASK_PRIORITY, 2);
+        myDatabase.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                null,
+                values,
+                SQLiteDatabase.CONFLICT_REPLACE);
+
+        values.put(TaskContract.TaskEntry.COL_TASK_TITLE, "Gym");
+        values.put(TaskContract.TaskEntry.COL_TASK_PRIORITY, 3);
+        myDatabase.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                null,
+                values,
+                SQLiteDatabase.CONFLICT_REPLACE);
+
+
+
+        //Retriving data from myDatabase. String s2 = myDatabase.retrieve()
+        ArrayList<String> taskList = new ArrayList<>();
+        ArrayList<Integer> priorityList = new ArrayList<>();
+
+        Cursor cursor = myDatabase.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE, TaskContract.TaskEntry.COL_TASK_PRIORITY},
+                null, null, null, null, TaskContract.TaskEntry.COL_TASK_PRIORITY + " DESC");
+        //get the task title and task priority and store them in taskList and priorityList
+        while(cursor.moveToNext()) {
+            int idx1 = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            int idx2 = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_PRIORITY);
+            taskList.add(cursor.getString(idx1));
+            priorityList.add(cursor.getInt(idx2));
+        }
+
+        String s1 = taskList.get(0);
+        assertEquals("Final Report", s1);
+        int priority1 = priorityList.get(0);
+        assertEquals(3, priority1);
+
+        String s2 = taskList.get(1);
+        int priority2 = priorityList.get(1);
+        assertEquals("Gym", s2);
+        assertEquals(3, priority2);
+
+        String s3 = taskList.get(2);
+        int priority3 = priorityList.get(2);
+        assertEquals("CS321 Test", s3);
+        assertEquals(2, priority3);
+
+        String s4 = taskList.get(3);
+        int priority4 = priorityList.get(3);
+        assertEquals("Pay my bill", s4);
+        assertEquals(1, priority4);
+
+        String s5 = taskList.get(4);
+        int priority5 = priorityList.get(4);
+        assertEquals("Sleep", s5);
+        assertEquals(1, priority5);
+
+        String s6 = taskList.get(5);
+        int priority6 = priorityList.get(5);
+        assertEquals("Landry", s6);
+        assertEquals(0, priority6);
     }
 
 }
